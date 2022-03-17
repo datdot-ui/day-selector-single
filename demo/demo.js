f29d0d98affb4decc9be5ca69db9baf2232f2057
Nina Breznik <ninabreznik@gmail.com>
2022-03-17 22:54:41 +0000

    Initial commit

---
+++ demo/demo.js
@@ -0,0 +1,149 @@
+const timelineDays = require('..')
+const csjs = require('csjs-inject')
+const bel = require('bel')
+const message_maker = require('message-maker')
+
+var id = 0
+
+function demo () {
+// ------------------------------------
+    const myaddress = `${__filename}-${id++}`
+    const inbox = {}
+    const outbox = {}
+    const recipients = {}
+    const names = {}
+    const message_id = to => (outbox[to] = 1 + (outbox[to]||0))
+
+    function make_protocol (name) {
+        return function protocol (address, notify) {
+            names[address] = recipients[name] = { name, address, notify, make: message_maker(myaddress) }
+            return { notify: listen, address: myaddress }
+        }
+    }
+    function listen (msg) {
+        console.log('New message', { msg })
+        const { head, refs, type, data, meta } = msg // receive msg
+        inbox[head.join('/')] = msg                  // store msg
+        const [from] = head
+        // send back ack
+        const { notify: from_notify, make: from_make, address: from_address } = names[from]
+        from_notify(from_make({ to: from_address, type: 'ack', refs: { 'cause': head } }))
+    }
+// ------------------------------------
+  const data = {
+    count: 3,
+    month:2,
+    year: 2022,
+    days: 29
+  }
+  const timelinedays = timelineDays( {data, style: `${css['timeline-days']}` }, make_protocol('calendar-days') )
+  const el = bel`<div class=${css.days}>
+    <h2 class=${css.title}>Timline days</h2>
+    <div class=${css['calendar-timeline-days']}>${timelinedays}</div>
+  </div>`
+
+  return el
+    
+}
+
+const css = csjs`
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: #F2F2F2;
+  height: 100%;
+}
+button:active, button:focus {
+  outline: dotted 1px #c9c9c9;
+}
+.wrap {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 75vh 25vh;
+  min-width: 520px
+}
+.terminal {
+  background-color: #212121;
+  color: #f2f2f2;
+  font-size: 13px;
+  padding: 0 20px;
+  overflow-y: auto;
+}
+.terminal div {
+  margin: 10px 0;
+}
+.terminal div:last-child {
+  color: #FFF500;
+  font-weight: bold;
+}
+.ui-widgets {
+  padding: 20px;
+  overflow-y: auto;
+}
+.ui-widgets > div {
+  margin-bottom: 30px;
+  padding: 10px 20px 20px 20px;
+  background-color: #fff;
+}
+.ui-tab {
+}
+.ui-tab > nav {
+  margin-bottom: 20px;
+}
+.title {
+  color: #008dff;
+}
+.ui-calendar-header {
+}
+.custom-header {
+  background-color: #f2f2f2;
+  max-width: 25%;
+  min-width: 225px;
+  border-radius: 50px;
+}
+.custom-header > [class^="calendar-header"] {
+  grid-template-rows: 30px;
+}
+.custom-header > [class^="calendar-header"] h3 {
+  font-size: 16px;
+}
+.calendar-header-fullsize {
+}
+.days {
+}
+.calendar-timeline-days {
+}
+.timeline-days {
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: repeat(auto-fit, minmax(30px, auto));
+  justify-content: left;
+  align-items: center;
+}
+.calendar-section {
+  margin-top: 30px;
+  font-size: 12px;
+}
+.calendar-table-days {
+  max-width: 210px;
+}
+.calendar-days {
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: repeat(7, minmax(30px, auto));
+  justify-items: center;
+}
+.calendar-weekday {
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: repeat(7, 30px);
+  justify-items: center;
+}
+.calendar-week {
+}
+.ui-datepicker {
+}
+`
+
+document.body.append(demo())
---
+++ src/index.js
@@ -0,0 +1,123 @@
+const debug = require('debug')
+const bel = require('bel')
+const message_maker = require('message-maker')
+const csjs = require('csjs-inject')
+const { format, getDate, getMonth, getYear, getDaysInMonth, isToday } = require('date-fns')
+
+var id = 0
+
+module.exports = datdot_ui_timeline_days
+
+function datdot_ui_timeline_days({from = 'Default', data = null, style}, parent_protocol) {
+
+// -----------------------------------
+  const myaddress = `${__filename}-${id++}`
+  const inbox = {}
+  const outbox = {}
+  const recipients = {}
+  const names = {}
+  const message_id = to => (outbox[to] = 1 + (outbox[to]||0))
+
+  const {notify, address} = parent_protocol(myaddress, listen)
+  names[address] = recipients['parent'] = { name: 'parent', notify, address, make: message_maker(myaddress) }
+  notify(recipients['parent'].make({ to: address, type: 'ready', refs: {} }))
+
+  function make_protocol (name) {
+      return function protocol (address, notify) {
+          console.log('PROTOCOL INIT', { name, address })
+          names[address] = recipients[name] = { name, address, notify, make: message_maker(myaddress) }
+          return { notify: listen, address: myaddress }
+      }
+  }
+
+  function listen (msg) {
+      const { head, refs, type, data, meta } = msg // receive msg
+      inbox[head.join('/')] = msg                  // store msg
+      const [from] = head
+      console.log('TIMELINE DAYS', { type, from, name: names[from].name, msg, data })
+      // handlers
+
+  }
+// -----------------------------------
+
+  const { make } = recipients['parent']
+  const log = debug(myaddress)
+  const date = new Date()
+  const today = getDate(date)
+
+  if ( data === null ) {
+      // if no data
+      var count = getMonth(date) // initial month
+      var month = format(date, 'MMMM') // get capitalization month
+      var year = getYear(date) // initial year
+      var days = getDaysInMonth(date) // initial days
+  } else {
+      // if data is loaded
+      var { from, count, month, year, days } = data
+  }
+
+  const is_today = (d) => isToday(new Date(year, count, d)) 
+  const el = bel`<section role="timeline-days" class=${style}></section>`
+
+  for (let d = 1; d <= days; d++ ) {
+      let date = format(new Date(year, count, d), 'd MMMM  yyyy, EEEE')
+      let setDate = format(new Date(year, count, d), 'yyyy-M-d')
+      
+      if (d < today && count <= new Date().getMonth() && year <= new Date().getFullYear() ) {
+          var day = bel`<div role="button" class="${css['timeline-day']} ${css['disabled-date']}" tabindex="-1" aria-today=${is_today(d)} aria-label="${date}" data-date="${setDate}">${d}</div>`
+      } else if ( count < new Date().getMonth() && year <= new Date().getFullYear() ) {
+          var day = bel`<div role="button" class="${css['timeline-day']} ${css['disabled-date']}" tabindex="-1" aria-today=${is_today(d)} aria-label="${date}" data-date="${setDate}">${d}</div>`
+      } else if ( year < new Date().getFullYear()) {
+          var day = bel`<div role="button" class="${css['timeline-day']} ${css['disabled-date']}" tabindex="-1" aria-today=${is_today(d)} aria-label="${date}" data-date="${setDate}">${d}</div>`
+      }
+        else {
+          var day = bel`<div role="button" class="${css['timeline-day']} ${is_today(d) ? `${css.today} ${css['date-selected']}` : ''}" tabindex="-1" aria-today=${is_today(d)} aria-label="${date}" aria-selected="${is_today(d)}" data-date="${setDate}" onclick=${(el) => onclick( el.target, setDate )}>${d}</div>`
+      }
+      el.append(day)
+  }
+
+  return el
+
+  function onclick(target, date) {
+    const children = [...el.children]
+    children.forEach( btn => {
+        btn.classList.remove(css['date-selected'])
+        btn.removeAttribute('aira-selected')
+    })
+
+    target.classList.add(css['date-selected'])
+    target.setAttribute('aria-selected', true)
+    return notify(make({ to: address, type: 'click', data: { body: date, count, month, year, days }}))
+  }
+}
+
+const css = csjs`
+.timeline-day {
+    text-align: center;
+    padding: 8px;
+    cursor: pointer;
+    transition: color 0.25s, background-color 0.25s ease-in-out;
+}
+.timeline-day:hover {
+    color: #fff;
+    background-color: #212121;
+}
+.timeline-day:focus, .timeline-day:active {
+    outline: dotted 1px #c9c9c9;
+}
+.today {
+    background-color: #f2f2f2;
+}
+.date-selected {
+    color: #fff;
+    background-color: #000;
+}
+.disabled-date {
+    color: #BBBBBB;
+    cursor: default;
+}
+.disabled-date:hover {
+    color: #BBBBBB;
+    background: none; 
+}
+`
