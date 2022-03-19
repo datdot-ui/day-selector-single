eacf21ce462031d7c15efbf226619c56f002b0a7
Nina Breznik <ninabreznik@gmail.com>
2022-03-19 18:31:21 +0000

    update bckgrnd color

--- bundle.js
+++ bundle.js
@@ -55,7 +55,6 @@ body {
   font-family: Arial, Helvetica, sans-serif;
   background-color: #F2F2F2;
   height: 100%;
-  background-color: red;
 }
 button:active, button:focus {
   outline: dotted 1px #c9c9c9;
